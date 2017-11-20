b9ee55c1147e3e768a31e3a8811064485d265303
mslowiak <marcin.slowiak.007@gmail.com>
2017-11-20 17:13:58 +0100

    ObserverIterator clear and sample in main

--- src/IteratorObservator/Main.java
+++ src/IteratorObservator/Main.java
@@ -6,15 +6,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> cos = new ObservableCollection<>(new ArrayList<>());
-        cos.add("Heja");
-        cos.add("Hop");
-        cos.add("bum sialalala");
-        cos.remove(1);
+        System.out.println("My list with observable");
+        List<String> list1 = new ObservableCollection<>(new ArrayList<>());
+        list1.add("jeden");
+        list1.add("dwa");
+        list1.add("trzy");
 
-        Iterator<String> iterator = cos.iterator();
-        while(iterator.hasNext()){
-            String y = iterator.next();
+        Iterator<String> iterator1 = list1.iterator();
+        while(iterator1.hasNext()){
+            String y = iterator1.next();
+            System.out.println(y);
+        }
+
+        list1.remove(1);
+
+
+        System.out.println("\n\nMy list with own iterator");
+        List<String> list2 = new MyCollectionWithOwnIterator<>(new ArrayList<>());
+        list2.add("jeden");
+        list2.add("dwa");
+        list2.add("trzy");
+
+        Iterator<String> iterator2 = list2.iterator();
+        while(iterator2.hasNext()){
+            String y = iterator2.next();
             System.out.println(y);
         }
     }
--- src/IteratorObservator/Observer.java
+++
@@ -1,6 +0,0 @@
-package IteratorObservator;
-
-public abstract class Observer<E> {
-    protected E element;
-    public abstract void update();
-}
